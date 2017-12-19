cf9261278123144f8e55917518197fd22e8c51ad
Medisetti Tanuja (Digital) <ME388491@wipro.com>
2017-12-19 19:10:53 +0530

    Adding Travis

---
+++ test/test1.js
@@ -0,0 +1,8 @@
+var assert = require("assert");
+describe('Array', function(){
+    describe('#indexOf()',function(){
+        it('should return -1 when the value if not present', function(){
+            assert.equal(-1, [1,2,3].indexOf(4));
+        })
+    })
+})
